010f443e2a952c5aab7aae0e76d497a3d439c78a
Ali Can Öztürk <acanozturk96@gmail.com>
2021-04-04 12:23:46 +0300

    Added pettypeservice to dataloader

--- pet-clinic-web/src/main/java/com/practice/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/practice/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.practice.petclinic.bootstrap;
 
 import com.practice.petclinic.model.Owner;
+import com.practice.petclinic.model.PetType;
 import com.practice.petclinic.model.Vet;
 import com.practice.petclinic.services.OwnerService;
+import com.practice.petclinic.services.PetTypeService;
 import com.practice.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +15,21 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Doggo");
+        petTypeService.save(dog);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -43,6 +51,6 @@ public class DataLoader implements CommandLineRunner {
         vet2.setLastName("Higgins");
         vetService.save(vet2);
 
-        System.out.println("Items saved.");
+        System.out.println("Bootstrap is loaded.");
     }
 }
