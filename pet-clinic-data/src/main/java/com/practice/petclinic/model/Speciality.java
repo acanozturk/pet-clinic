880ae24289069506d7bdd2895a4995abda19136b
Ali Can Öztürk <acanozturk96@gmail.com>
2021-04-04 11:46:13 +0300

    Added vet speciality class

---
+++ pet-clinic-data/src/main/java/com/practice/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.practice.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String speciality;
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
