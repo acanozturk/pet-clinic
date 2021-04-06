e3b0ed87d7e6e24f8309f234cff7b2b8d520f202
Ali Can Öztürk <acanozturk96@gmail.com>
2021-04-06 21:28:37 +0300

    Added entity relations

--- pet-clinic-data/src/main/java/com/practice/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/practice/petclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package com.practice.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "breed")
     private String breed;
 
     public String getBreed() {
