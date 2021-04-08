f33ea6e47dbcf8fbabac3f3fc83dd6c20033dadf
Ali Can Öztürk <acanozturk96@gmail.com>
2021-04-08 18:33:52 +0300

    update

--- pet-clinic-web/src/main/java/com/practice/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/practice/petclinic/bootstrap/DataLoader.java
@@ -22,12 +22,8 @@ public class DataLoader implements CommandLineRunner {
     private final VisitService visitService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService,
-                      PetService petService,
-                      PetTypeService petTypeService,
-                      VetService vetService,
-                      SpecialityService specialityService,
-                      VisitService visitService) {
+    public DataLoader(OwnerService ownerService, PetService petService, PetTypeService petTypeService,
+                      VetService vetService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.petService = petService;
         this.petTypeService = petTypeService;
