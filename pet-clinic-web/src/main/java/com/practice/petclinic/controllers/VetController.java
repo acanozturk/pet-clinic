f5b736d9b6c28334afaeec095a0835de72585e2f
Ali Can Öztürk <acanozturk96@gmail.com>
2021-04-12 15:16:15 +0300

    Added getjson for vetController

--- pet-clinic-web/src/main/java/com/practice/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/practice/petclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.practice.petclinic.controllers;
 
+import com.practice.petclinic.model.Vet;
 import com.practice.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -15,9 +20,15 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/vets.html"})
+    @RequestMapping({"", "/vets", "/vets.html"})
     public String listOfVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/vetList";
     }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
+
 }
